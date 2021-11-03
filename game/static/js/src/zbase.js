fe9ce0f2d93a4509f23f758bc91ee0f271cc16b0
彭于晏 <1973185277@qq.com>
2021-11-03 12:12:04 +0800

    write menu and playground scene

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,4 +1,13 @@
-class ACGame {
+class AcGame {
     constructor(id) {
+        this.id = id;
+        this.$ac_game = $('#' + id);
+        this.menu = new AcGameMenu(this);
+        this.playground = new AcGamePlayground(this);
+
+        this.start();
+    }
+
+    start() {
     }
 }
