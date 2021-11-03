dac5fe9194afa540276ba14b10f79b94a2a98a39
彭于晏 <1973185277@qq.com>
2021-11-03 09:24:37 +0800

    Modify global settings

--- game/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from game.views import gyr,index,play
-
-urlpatterns = {
-    path("gyr/", gyr, name="gyr"),
-    path("", index, name="index"),
-    path("play/", play, name="play"),
-}
